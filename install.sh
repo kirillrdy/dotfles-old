1ff79aa854e5de0d385036c3fd8962d40ff97b22
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2013-01-18 16:06:36 +0000

    adjusted install script to install new vimrc

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 echo "Installing vimrc into ~/.vimrc"
-cp -v  vimrc ~/.vimrc
+cp -v  dot.vimrc ~/.vimrc
 cp -v  Xdefaults  ~/.Xdefaults
 mkdir -pv ~/.vim
 cp -rv vim/* ~/.vim/
 
 
-cp gitconfig ~/.gitconfig
-cp tigrc ~/.tigrc
+cp -v gitconfig ~/.gitconfig
+cp -v tigrc ~/.tigrc
