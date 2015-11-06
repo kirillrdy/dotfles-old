58948549c0bc8e552a6dc1cb7a8764a956db2154
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-11-07 10:27:52 +1100

    Slowly phasing out vim

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-echo "Installing vimrc into ~/.vimrc"
-cp -v  dot.vimrc ~/.vimrc
-cp -v  dot.xinitrc ~/.xinitrc
-cp -v  Xdefaults  ~/.Xdefaults
-mkdir -pv ~/.vim
-cp -rv vim/* ~/.vim/
-
-
 cp -v dot.gitconfig ~/.gitconfig
 cp -v tigrc ~/.tigrc
 cp -v dot.spacemacs ~/.spacemacs
