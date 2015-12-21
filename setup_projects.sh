728368d35365e7214dba65fade6487c362a691c5
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2015-12-21 10:17:29 +0900

    Added gopherjs to list of projects

--- setup_projects.sh
+++ setup_projects.sh
@@ -5,7 +5,9 @@ set -ex
 root_dir=$HOME/src/github.com/kirillrdy
 mkdir -p $root_dir
 cd $root_dir
+#TODO add clone or update option
 git clone git@github.com:kirillrdy/nadeshiko.git
 git clone git@github.com:kirillrdy/vidos.git
 git clone git@github.com:kirillrdy/libkanji.git
 git clone git@github.com:kirillrdy/silverkanji.git
+git clone git@github.com:kirillrdy/gopherjs.git
