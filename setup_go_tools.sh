d2d22ea1440d3502c86df15f2a7030b80ef42cb7
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2016-03-25 12:02:55 +0900

    added errcheck

--- setup_go_tools.sh
+++ setup_go_tools.sh
@@ -3,3 +3,4 @@ go get -v -u github.com/nsf/gocode
 go get -v -u github.com/rogpeppe/godef
 go get -v -u golang.org/x/tools/cmd/gorename
 go get -v -u golang.org/x/tools/cmd/oracle
+go get -v -u github.com/kisielk/errcheck
